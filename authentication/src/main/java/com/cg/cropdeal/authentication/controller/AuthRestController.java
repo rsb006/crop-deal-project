810ff387ea393926849408412ba4a04f49fe5072
Anmol Sharma <anmolsh.1908@gmail.com>
2022-08-20 10:49:12 +0530

    Registered authentication microservice as eureka client

---
+++ authentication/src/main/java/com/cg/cropdeal/authentication/controller/AuthRestController.java
@@ -0,0 +1,13 @@
+package com.cg.cropdeal.authentication.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthRestController {
+
+	@GetMapping("/")
+	public String helloWorld() {
+		return "Hello World 1";
+	}
+}
