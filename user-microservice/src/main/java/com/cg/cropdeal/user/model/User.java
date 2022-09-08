e438aaf8c9f0e3e332acb3ad466ea00dc7885bda
rsb006 <rutujabhoite369@gmail.com>
2022-09-08 18:08:54 +0530

    added one to many mapping for cropitems

--- user-microservice/src/main/java/com/cg/cropdeal/user/model/User.java
+++ user-microservice/src/main/java/com/cg/cropdeal/user/model/User.java
@@ -1,12 +1,17 @@
 package com.cg.cropdeal.user.model;
 
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -18,6 +23,7 @@ public class User {
 	 @ApiModelProperty(notes = "unique id of user")
 	  @Id
 	  @GeneratedValue(strategy = GenerationType.AUTO)
+	 @Column(name ="user_id" )
       private Long userId ;
 	
 	 @ApiModelProperty(notes = "The user's full name")
@@ -55,6 +61,9 @@ public class User {
 	  //@JoinColumn(name="address_id")
 	   @ApiModelProperty(notes = "Address of user")
 	   Address address;
+	   
+	   @OneToMany(cascade = CascadeType.ALL)
+	   private List<cropItem> cropItem;
 
 	    public Long getUserId() {
 		    return userId;
